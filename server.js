5bb3a80cbbdae45cea405a3716b0802772f7bdc6
Denis <deniska.vla@gmail.com>
2020-11-23 12:32:36 +0200

    add image way

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('country.json');
+const routerImages = jsonServer.router('image');
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3010;
 
 server.use(middlewares);
-server.use(router);
+server.use(router, routerImages);
 
 server.listen(port);
