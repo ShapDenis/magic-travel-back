ea41f4cd71314b06553eaf7899a794af0f1a7254
Denis <deniska.vla@gmail.com>
2020-11-23 13:20:37 +0200

    fix Id country.json

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('country.json');
-server.use(jsonServer.rewriter({
-    '/image/:id': '/assets/images/Img_1.png'
-}))
+// server.use(jsonServer.rewriter({
+//     '/image/:id': './public/assets/images/Img_1.png'
+// }))
 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3010;
